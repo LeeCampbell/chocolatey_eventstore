3f867fd1ecaad29c025718cd7f7fce200bbf2d0d
Owain Perry <owain.perry@justgiving.com>
2014-04-03 13:34:02 +0100

    ensure nuget download

--- build.ps1
+++ build.ps1
@@ -1,7 +1,13 @@
 param([string]$task, [string]$version="unset")
 
 
-.\nuget.exe install psake -ExcludeVersion -OutputDirectory "temp"
+if ((Test-Path ".\nuget.exe") -eq $false)
+{
+    $webClient = new-object net.webclient
+    $webClient.DownloadFile('https://github.com/OwainPerry/nuget.commandline/raw/master/NuGet.exe','nuget.exe')
+}
+
+.\nuget.exe install psake  -ExcludeVersion -OutputDirectory "packages"
 
 
 $packageName = (Get-Item .).name	#use the folder name as the package name (overrride if relevant)
@@ -11,5 +17,5 @@ $authors = "owain perry"    	#Set this
 $summary = "$packageName"		#Set this. 
 $tags 	= "$packageName"		#Set this. 	 
 
-Import-Module '.\temp\psake\tools\psake.psm1'; 
+Import-Module '.\packages\psake\tools\psake.psm1'; 
 Invoke-psake  .\default.ps1 -t $task -parameters @{"packageName"=$packageName;"id"=$id;"title"=$title;"authors"=$authors;"summary"=$summary;"tags"=$tags;"version"=$version}
